0f3b23f8d24a5a48d9a70d2514fb7407306dd470
DeemoHarlos <cces4415@gmail.com>
2018-03-27 15:53:02 +0800

    fix text

--- diary.js
+++ diary.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const md2html = require('./markdown2html')
 var data = require('./diary/data')
 var app = require('express').Router()
 
@@ -11,10 +10,9 @@ app.get('/:id',(req,res)=>{
 	var target = data.find(e=>(e.id === req.params.id))
 	if (!target) res.status(404).send('Diary #' + req.params.id + 'Not found.')
 	var md = fs.readFileSync('diary/data/' + req.params.id + '.md','utf8')
-	var converter = new md2html.Converter()	
-    var html = converter.makeHtml(md)
-	target.content = html
-	res.json(target)
+	var response = {}
+	response.content = md
+	res.json(response)
 })
 
 module.exports = app	
--- diary/data.js
+++ diary/data.js
@@ -1,10 +1,10 @@
 module.exports = [
 	{'id':'0001','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0001','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0001','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0001','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0001','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0001','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0001','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0001','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']}
+	{'id':'0002','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
+	{'id':'0003','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
+	{'id':'0004','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
+	{'id':'0005','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
+	{'id':'0006','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
+	{'id':'0007','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
+	{'id':'0008','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']}
 ]
