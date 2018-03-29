3553266f20f15033cffa5c87eb53f59af194ed78
DeemoHarlos <cces4415@gmail.com>
2018-03-30 00:53:21 +0800

    update

--- data/diary/data.js
+++ data/diary/data.js
@@ -1,10 +1,9 @@
 module.exports = [
-	{'id':'0001','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0002','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0003','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0004','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0005','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0006','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0007','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']},
-	{'id':'0008','title':'TEST','written':new Date(2018, 3,13, 0,43,47),'tag':['TEST']}
+	{'id':'0001','title':'2018.03.12','written':new Date(2018, 3,13, 0,43,47),'tag':[]},
+	{'id':'0002','title':'SITCON 2018','written':new Date(2018, 3,11, 2,57,0),'tag':[]},
+	{'id':'0003','title':'SITCON 2018 前夕','written':new Date(2018, 3,10, 0,46,0),'tag':[]},
+	{'id':'0004','title':'返家 - 惡夢 (未完)','written':new Date(2018, 1, 31, 21,21,0),'tag':[]},
+	{'id':'0005','title':'2018.02.04','written':new Date(2018, 2, 5, 4,23,0),'tag':[]},
+	{'id':'0006','title':'2018.01.29 管樂交流 @ 台南','written':new Date(2018, 1, 29, 3,5,0),'tag':[]}
+	{'id':'0007','title':'2018.01.27 管樂團練 @ 新竹','written':new Date(2018, 1, 27, 7,54,0),'tag':[]}
 ]
